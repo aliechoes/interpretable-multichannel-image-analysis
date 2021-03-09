624d217571b68c05dc617d4417b4202c3a3b1850
Aleksandra.Kornivetc <6kornive@informatik.uni-hamburg.de>
2021-03-09 13:30:11 +0100

    script to train model

---
+++ dataset.py
@@ -0,0 +1,171 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import h5py
+from sklearn.model_selection import train_test_split
+from skimage.util import crop,  random_noise
+import copy
+import sys
+sys.path.append("..")
+
+def crop_pad_h_w(image_dummy,reshape_size):
+    if image_dummy.shape[0] < reshape_size:
+        h1_pad = ( reshape_size - image_dummy.shape[0])/2
+        h1_pad = int(h1_pad)
+        h2_pad =  reshape_size - h1_pad - image_dummy.shape[0]
+        h1_crop = 0
+        h2_crop = 0
+    else:
+        h1_pad = 0
+        h2_pad = 0
+        h1_crop = ( reshape_size - image_dummy.shape[0])/2
+        h1_crop = abs(int(h1_crop))
+        h2_crop = image_dummy.shape[0]- reshape_size  - h1_crop
+
+    if image_dummy.shape[1] < reshape_size:
+        w1_pad = (reshape_size - image_dummy.shape[1])/2
+        w1_pad = int(w1_pad)
+        w2_pad = reshape_size - w1_pad - image_dummy.shape[1]
+        w1_crop = 0
+        w2_crop = 0
+    else:
+        w1_pad = 0
+        w2_pad = 0
+        w1_crop = (reshape_size - image_dummy.shape[1])/2
+        w1_crop = abs(int(w1_crop))
+        w2_crop = image_dummy.shape[1]- reshape_size  - w1_crop
+
+    h = [h1_crop, h2_crop, h1_pad, h2_pad]
+    w = [w1_crop, w2_crop, w1_pad, w2_pad]
+    return h, w
+
+def train_validation_test_split(h5_file, validation_size = 0.15, test_size = 0.20, only_classes = None):
+    data = h5py.File(h5_file, "r")
+    # object_numbers = data.get("object_number")[()]
+    if only_classes is None:
+        only_classes = data.get("labels")[()]
+    object_numbers = data.get("object_number")[()][np.isin(data.get("labels")[()], only_classes)]
+    data.close()
+    train, test = train_test_split( object_numbers,
+                                       test_size=test_size,
+                                       random_state=314)
+    train, validation = train_test_split( train,
+                                       test_size=validation_size,
+                                       random_state=314)
+    return train, validation, test
+
+def get_classes_map(h5_file):
+    data = h5py.File(h5_file, "r")
+    label_map = data.get("label_map")[()]
+    data.close()
+    return eval(label_map)
+
+def number_of_channels(h5_file, only_channels = []):
+    if len(only_channels)==0:
+        data = h5py.File(h5_file, "r")
+        object_numbers = data.get("object_number")[()]
+        o_n = object_numbers[0]
+        num_channels = data.get(str(o_n)  + "_image"  )[()].shape[2]
+        data.close()
+        return num_channels
+    else:
+        return len(only_channels)
+
+def number_of_classes(h5_file, only_classes = None):
+    if only_classes is not None:
+        return len(only_classes)
+    else:
+        data = h5py.File(h5_file, "r")
+        labels = data.get("labels")[()]
+        num_classes = len(set(labels))
+        data.close()
+        return num_classes
+
+
+class Dataset_Generator(Dataset):
+
+    def __init__(self, h5_file, set_indx, scaling_factor=4095.,
+                 reshape_size=64, data_map=[], statistics=None,
+                 transform=None, means=None, stds=None,
+                 only_channels=[], only_classes=None):
+
+        self.data = h5py.File(h5_file, "r")
+
+        object_numbers = self.data.get("object_number")[()]
+
+        try:
+            labels = self.data.get("labels")[()]
+        except TypeError as TE:
+            labels = np.array(len(object_numbers) * [-1])
+
+        self.only_channels = only_channels
+        self.only_classes = only_classes
+        self.object_numbers = object_numbers[set_indx]
+        self.labels = labels[set_indx]
+
+        self.num_channels = number_of_channels(h5_file, self.only_channels)
+        self.scaling_factor = scaling_factor
+        self.reshape_size = reshape_size
+        self.data_map = data_map
+        self.statistics = statistics
+        self.transform = transform
+        if means is None:
+            self.means = torch.zeros(self.num_channels)
+        else:
+            self.means = means
+        if stds is None:
+            self.stds = torch.ones(self.num_channels)
+        else:
+            self.stds = stds
+
+    def __len__(self):
+        return len(self.object_numbers)
+
+    def __getitem__(self, idx):
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        o_n = self.object_numbers[idx]
+        lbl = self.labels[idx]
+
+        try:
+            image_original = self.data.get(str(o_n) + "_image")[()] / self.scaling_factor
+            # image_original = (image_original - self.means) / self.stds
+            # creating the image
+            h, w = crop_pad_h_w(image_original, self.reshape_size)
+            h1_crop, h2_crop, h1_pad, h2_pad = h
+            w1_crop, w2_crop, w1_pad, w2_pad = w
+            image = np.zeros((self.num_channels, self.reshape_size, self.reshape_size), dtype=np.float64)
+            nmb_of_channels = 0
+            # filling the image with different channels
+            for ch in range(image_original.shape[2]):
+                if len(self.only_channels) == 0 or ch in self.only_channels:
+                    image_dummy = crop(image_original[:, :, ch], ((h1_crop, h2_crop), (w1_crop, w2_crop)))
+                    image_dummy = np.pad(image_dummy, ((h1_pad, h2_pad), (w1_pad, w2_pad)), "edge")
+                    image[nmb_of_channels, :, :] = image_dummy
+                    nmb_of_channels += 1
+
+            image_original = None
+            # map numpy array to tensor
+            image = torch.from_numpy(copy.deepcopy(image))
+
+            for i in range(self.num_channels):
+                image[i] = (image[i] - self.means[i]) / self.stds[i]
+
+            if self.transform:
+                image = self.transform(image)
+
+            if self.only_classes is not None:
+                lbl = self.only_classes.index(lbl)
+            label = np.array([lbl])
+
+            object_number = np.array([o_n])
+
+            sample = {'image': image, 'label': torch.from_numpy(label), "idx": idx, "object_number": object_number}
+        except:
+            sample = {'image': torch.from_numpy(
+                np.zeros((self.num_channels, self.reshape_size, self.reshape_size), dtype=np.float64)),
+                      'label': torch.from_numpy(np.array([-1])), "idx": idx, "object_number": np.array([-1])}
+
+        return sample
---
+++ train.py
@@ -0,0 +1,223 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models
+import argparse
+from datetime import datetime
+import os
+import time
+import multiprocessing
+import psutil
+import json
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import h5py
+from sklearn.model_selection import train_test_split
+from skimage.util import crop, random_noise
+from skimage.transform import rescale, resize, rotate, AffineTransform, warp
+import torch.optim as optim
+from tqdm import tqdm
+import copy
+import sys
+
+sys.path.append("..")
+import logging
+import random
+
+from resnet18 import resnet18
+from dataset import Dataset_Generator, train_validation_test_split, get_classes_map, number_of_classes, \
+    number_of_channels
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--h5_file', default="data/Lyse fix sample_1_Focused & Singlets & CD45 pos.h5",
+                    help="dataset root dir")
+parser.add_argument('--batch_size', default=64, help="batch size", type=int)
+parser.add_argument('--n_epochs', default=10, help="epochs to train", type=int)
+parser.add_argument('--num_workers', type=int, default=2, help='number of data loading workers')
+parser.add_argument('--lr', default=0.001, help="learning rate", type=float)
+parser.add_argument('--model_save_path', default='models/', help="path to save models")
+parser.add_argument('--log_dir', default='logs/', help="path to save logs")
+parser.add_argument('--resume_model', default='', help="resume model name")
+parser.add_argument('--only_channels', default=[], help="the channels to be used for the model training", nargs='+', type=int)
+parser.add_argument('--only_classes', default=None, help="the classes to be used for the model training", nargs='+', type=int)
+opt = parser.parse_args()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def get_statistics(dataloader):
+    nmb_channels = 0
+    if len(opt.only_channels) == 0:
+        nmb_channels = 12
+    else:
+        nmb_channels = len(opt.only_channels)
+
+    statistics = dict()
+    statistics["mean"] = torch.zeros(nmb_channels)
+    statistics["std"] = torch.zeros(nmb_channels)
+
+    for j, data in enumerate(dataloader, 1):
+
+        data = data["image"]
+        for i in range(nmb_channels):
+            statistics["mean"][i] += data[:, i, :, :].mean()
+            statistics["std"][i] += data[:, i, :, :].std()
+    return statistics
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(filename=os.path.join(opt.log_dir, 'output.txt'), level=logging.DEBUG)
+
+    train_indx, validation_indx, test_indx = train_validation_test_split(h5_file=opt.h5_file, only_classes=opt.only_classes)
+
+    label_map = get_classes_map(opt.h5_file)
+
+    transform = transforms.Compose(
+        [transforms.RandomVerticalFlip(),
+         transforms.RandomHorizontalFlip(),
+         transforms.RandomRotation(45)])
+
+    train_dataset = Dataset_Generator(opt.h5_file, train_indx, reshape_size=64, transform=transform,
+                                      only_channels=opt.only_channels, only_classes=opt.only_classes)
+    trainloader = DataLoader(train_dataset,
+                             batch_size=opt.batch_size,
+                             shuffle=False,
+                             num_workers=opt.num_workers)
+
+    # collect statistics of the train data (mean & standard deviation) to normalize the data
+    statistics = get_statistics(trainloader)
+
+    # create a new normalized train_dataset
+    train_dataset = Dataset_Generator(opt.h5_file, train_indx, reshape_size=64, transform=transform,
+                                      means=statistics["mean"].div_(len(trainloader)),
+                                      stds=statistics["std"].div_(len(trainloader)), only_channels=opt.only_channels,
+                                      only_classes=opt.only_classes)
+    validation_dataset = Dataset_Generator(opt.h5_file, validation_indx, reshape_size=64,
+                                           means=statistics["mean"].div_(len(trainloader)),
+                                           stds=statistics["std"].div_(len(trainloader)),
+                                           only_channels=opt.only_channels, only_classes=opt.only_classes)
+    test_dataset = Dataset_Generator(opt.h5_file, test_indx, reshape_size=64,
+                                     means=statistics["mean"].div_(len(trainloader)),
+                                     stds=statistics["std"].div_(len(trainloader)), only_channels=opt.only_channels,
+                                     only_classes=opt.only_classes)
+
+    trainloader = DataLoader(train_dataset,
+                             batch_size=opt.batch_size,
+                             shuffle=False,
+                             num_workers=opt.num_workers)
+    validationloader = DataLoader(validation_dataset,
+                                  batch_size=opt.batch_size,
+                                  shuffle=False,
+                                  num_workers=1)
+    testloader = DataLoader(test_dataset,
+                            batch_size=opt.batch_size,
+                            shuffle=False,
+                            num_workers=1)
+
+    logging.info('train dataset: %d, validation dataset: %d, test dataset: %d' % (
+        len(train_dataset), len(validation_dataset), len(test_dataset)))
+
+    num_classes = number_of_classes(opt.h5_file, only_classes=opt.only_classes)
+    num_channels = number_of_channels(opt.h5_file, only_channels=opt.only_channels)
+    logging.info('used only channels: %s; only classes: %s' % (str(opt.only_channels), str(opt.only_classes)))
+
+    model = resnet18(pretrained=True)
+
+    # loading the imagenet weights in case it is possible
+    if num_channels != 3:
+        model.conv1 = nn.Conv2d(num_channels, 64, kernel_size=(7, 7),
+                                stride=(2, 2), padding=(3, 3), bias=False)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, num_classes)
+
+    model = model.to(device)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=0.9)
+
+    if opt.resume_model != '':
+        checkpoint = torch.load('{0}/{1}'.format(opt.model_save_path, opt.resume_model))
+        model.load_state_dict(checkpoint)
+        for log in os.listdir(opt.log_dir):
+            os.remove(os.path.join(opt.log_dir, log))
+
+    for epoch in range(opt.n_epochs):
+        running_loss = 0.0
+        logging.info('epoch%d' % epoch)
+        for i, data in enumerate(trainloader, 0):
+
+            # get the inputs; data is a list of [inputs, labels]
+            indx = (data["object_number"] != -1).reshape(-1)
+            if indx.sum() > 0:
+                inputs, labels = data["image"][indx], data["label"][indx]
+
+                inputs, labels = inputs.to(device), labels.to(device)
+                inputs = inputs.float()
+                labels = labels.reshape(-1)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+
+                loss = criterion(outputs, F.one_hot(labels.long(), num_classes).type_as(outputs))
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+            if i % 500 == 499:  # print every 2000 mini-batches
+                logging.info('[%d, %5d] training loss: %.8f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for i, data in enumerate(validationloader, 0):
+                #breakpoint()
+                indx = (data["object_number"] != -1).reshape(-1)
+                if indx.sum() > 0:
+                    inputs, labels = data["image"][indx], data["label"][indx]
+
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    inputs = inputs.float()
+                    labels = labels.reshape(-1)
+
+                    outputs = model(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (labels.reshape(-1) == predicted).sum().item()
+
+        logging.info('Accuracy of the network on the %d validation images: %d %%' % (
+            len(validation_dataset), 100 * correct / total))
+
+    logging.info('Finished Training')
+
+    correct = 0.
+    total = 0.
+
+    with torch.no_grad():
+        for data in testloader:
+            indx = (data["object_number"] != -1).reshape(-1)
+            if indx.sum() > 0:
+                inputs, labels = data["image"][indx], data["label"][indx]
+
+                inputs, labels = inputs.to(device), labels.to(device)
+                inputs = inputs.float()
+                labels = labels.reshape(-1)
+
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (labels.reshape(-1) == predicted).sum().item()
+
+    logging.info('Accuracy of the network on the %d test images: %d %%' % (len(test_dataset),
+                                                                           100 * correct / total))
+
+    torch.save(model.state_dict(), os.path.join(opt.model_save_path, "final_model_dict.pth"))
